04526dcb69d91195141f4db75767d2320bb03f90
maynak-dev <work.maynak@gmail.com>
2026-02-17 17:00:35 +0530

    build.sh corrected

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py ensure_superuser   
+python manage.py migrate             
+python manage.py ensure_superuser      
